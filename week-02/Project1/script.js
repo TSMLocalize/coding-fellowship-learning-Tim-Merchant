03a31a47e22e4292b5d6a6fc79821100655a2d32
Timothy <ryalin1286@gmail.com>
2021-01-14 17:32:09 +0000

    removed failed javascript lol

--- week-02/Project1/script.js
+++ week-02/Project1/script.js
@@ -1,17 +1,17 @@
-let burgerBtn = document.getElementById('burgerBtn');
-let burgerList = document.getElementById('burgerList');
+// let burgerBtn = document.getElementById('burgerBtn');
+// let burgerList = document.getElementById('burgerList');
 
-burgerBtn.addEventListener('click', function(){
+// burgerBtn.addEventListener('click', function(){
     
-    if (burgerList.classList.contains('active')) {
+//     if (burgerList.classList.contains('active')) {
         
-        burgerList.classList.value = "burgerList animateBurgerDown";                        
-    } else {
-        burgerList.classList.value = "burgerList active animateBurgerUp";                            
-    }
+//         burgerList.classList.value = "burgerList animateBurgerDown";                        
+//     } else {
+//         burgerList.classList.value = "burgerList active animateBurgerUp";                            
+//     }
 
-});
+// });
 
-if (window.screen.width > 780) {
-    burgerList.classList.value = "";                        
-}
+// if (window.screen.width > 780) {
+//     burgerList.classList.value = "";                        
+// }
